e42bbe9ec0700e9dd6fc32a695be9f77fb3905f3
Mateusz Oko <mo89mo@gmail.com>
2023-03-24 16:54:56 +0100

    Add 404 NotFound Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import Favorite from './components/Favorite/Favorite';
 import About from './components/About/About';
 import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/favorite" element={<Favorite />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
       </Container>
     </main>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import PageTitle from "../PageTitle/PageTitle"
+
+const NotFound = () => {
+    return (
+        <div>
+            <PageTitle>Error 404</PageTitle>
+            <PageTitle>Page not found!</PageTitle>
+        </div>
+    )
+}
+
+export default NotFound
