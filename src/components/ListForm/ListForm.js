ca7bd04ce9956d56bb3a7da74fd530a05c54d883
Mateusz Oko <mo89mo@gmail.com>
2023-03-28 23:24:24 +0200

    Add ListForm

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,30 @@
+import styles from './ListForm.module.scss';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addList } from '../../redux/store';
+
+const ListForm = props => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addList({title, description}));
+        setTitle('');
+        setDescription('');
+    }
+
+    return (
+        <form className={styles.listForm} onSubmit={handleSubmit}>
+            <span>Title:</span><TextInput value={title} placeholder="Title" onChange={e => setTitle(e.target.value)} />
+            <span>Description:</span><TextInput value={description} placeholder="Description" onChange={e => setDescription(e.target.value)} />
+            <Button>Add list</Button>
+        </form>
+    )
+}
+
+export default ListForm;
