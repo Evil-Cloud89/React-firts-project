ee0229710251b12554790375d5908dc95b5b70d0
Mateusz Oko <mo89mo@gmail.com>
2023-03-30 14:12:30 +0200

    Add favorite cards to their page

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,10 +1,27 @@
+import styles from './Favorite.module.scss';
 import PageTitle from "../PageTitle/PageTitle";
+import Card from '../Card/Card';
+import { useSelector } from 'react-redux';
+import { getFavoriteCard } from '../../redux/store';
 
 const Favorite = () => {
+    const favoriteCards = useSelector(getFavoriteCard);
+    if (favoriteCards.length === 0) {
+        return(
+            <div className={styles.favorite}>
+              <PageTitle>No favorive cards</PageTitle>
+            </div>
+        );
+    }
+
     return (
         <div>
             <PageTitle>Favorite</PageTitle>
-            <p>Lorem ipsum.</p>
+            <div className={styles.column}>
+                <ul className={styles.favoriteCards}>
+                    {favoriteCards.map(card => <Card key={card.id} title={card.title} isFavorite={card.isFavorite} />)}
+                </ul>
+            </div>
         </div>
     )
 }
